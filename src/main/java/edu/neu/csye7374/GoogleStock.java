3589dec48b828f6de9c6a6d04c1af75bec31b2d6
Eashan Roy <roy.ea@northeastern.edu>
2025-01-19 17:23:17 -0500

    feat: two sub classes from StockAPI

--- src/main/java/edu/neu/csye7374/GoogleStock.java
+++ src/main/java/edu/neu/csye7374/GoogleStock.java
@@ -1,2 +1,20 @@
-package edu.neu.csye7374;public class GoogleStock {
+package edu.neu.csye7374;
+
+public class GoogleStock extends StockAPI{
+    public GoogleStock(String name, double price, String description) {
+        super(name, price, description);
+    }
+
+    @Override
+    public void setPrice(double price) {
+        if(price> this.getPrice()) {
+            int value = (int) ((price-this.getPrice())*1.5/this.getPrice());
+            setMetric(this.getMetric() +value);
+        }else {
+            int value = (int) ((price-this.getPrice())*0.9/this.getPrice());
+            setMetric(this.getMetric() +value);
+        }
+        this.price = price;
+    }
+
 }
